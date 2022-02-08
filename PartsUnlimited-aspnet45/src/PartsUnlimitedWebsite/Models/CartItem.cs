eb5177c74a93cf13bcd61af4cf61cdc4ccce6d23
Samer Sahih <samersahih@gmail.com>
2022-02-08 12:50:59 +0300

    testing

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // First change again
+        // First change new
         [Key]
         public int CartItemId { get; set; }
 
